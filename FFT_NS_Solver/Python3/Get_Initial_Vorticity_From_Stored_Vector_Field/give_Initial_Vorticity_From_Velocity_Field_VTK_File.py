d30fa57669e92b89530253e5e62a237561fb24ca
Nick Battista <nickbattista@Nicks-MacBook-Air.local>
2020-01-30 10:52:53 -0500

    get rid of erronenus functions

--- FFT_NS_Solver/Python3/Get_Initial_Vorticity_From_Stored_Vector_Field/give_Initial_Vorticity_From_Velocity_Field_VTK_File.py
+++ FFT_NS_Solver/Python3/Get_Initial_Vorticity_From_Stored_Vector_Field/give_Initial_Vorticity_From_Velocity_Field_VTK_File.py
@@ -6,13 +6,8 @@
 #
 ###########################################################################
 
-#from pathlib import Path
 import numpy as np
 import os
-#import vtk
-#from vtk.util import numpy_support
-#from vtk.numpy_interface import dataset_adapter as dsa
-
 
 #################################################################################
 #
@@ -149,75 +144,3 @@ def give_Initial_Vorticity_From_Velocity_Field_VTK_File():
     initial_vorticity = np.transpose(initial_vorticity)
 
     return initial_vorticity
-
-# ###########################################################################
-# #
-# # FUNCTION: Reads in the velocity data field from .vtk format
-# #
-# ###########################################################################
-
-# def read_Eulerian_Velocity_Field_vtk(simNums, xy=False):
-#     '''This is to read Eulerian IB2d data, either scalar or vector.'''
-
-#     #filename = Path(path) / (strChoice + '.' + str(simNums) + '.vtk')
-#     filename = 'u.' + str(simNums) + '.vtk'
-#     data = read_vtk_Structured_Points(str(filename))
-
-#     if xy:
-#         # reconstruct mesh
-#         origin = data[-2]
-#         spacing = data[-1]
-#         x = np.arange(data[0].shape[0])*spacing[0]+origin[0]
-#         y = np.arange(data[0].shape[1])*spacing[1]+origin[1]
-
-#     # infer if it was a vector or not and return accordingly
-#     if len(data) == 3:
-#         # scalar
-#         if xy:
-#             return data[0], x, y
-#         else:
-#             return data[0]
-#     else:
-#         # vector
-#         if xy:
-#             return data[0], data[1], x, y
-#         else:
-#             return data[0], data[1]
-
-
-
-# def read_vtk_Structured_Points(filename):
-#     '''This will read in either Scalar or Vector data!'''
-
-#     # Check for valid filename (vtk crashes badly if filename is invalid)
-#     if not Path(filename).exists():
-#         raise OSError(2, "No such file", filename)
-
-#     # Load data
-#     reader = vtk.vtkStructuredPointsReader()
-#     reader.SetFileName(filename)
-#     reader.Update()
-#     vtk_data = reader.GetOutput()
-
-#     # Get mesh data
-#     mesh_shape = vtk_data.GetDimensions()
-#     origin = vtk_data.GetOrigin()
-#     spacing = vtk_data.GetSpacing()
-
-#     # Read in data
-#     scalar_data = vtk_data.GetPointData().GetScalars()
-#     if scalar_data is not None:
-#         np_data = numpy_support.vtk_to_numpy(scalar_data)
-#         e_data = np.reshape(np_data, mesh_shape[::-1]).squeeze()
-#         # Indexed [z,y,x] since x changes, then y, then z in the flattened array
-#         return e_data, origin, spacing
-#     else:
-#         vector_data = vtk_data.GetPointData().GetVectors()
-#         np_data = numpy_support.vtk_to_numpy(vector_data)
-#         e_data_X = np.reshape(np_data[:,0], mesh_shape[::-1]).squeeze()
-#         e_data_Y = np.reshape(np_data[:,1], mesh_shape[::-1]).squeeze()
-#         e_data_Z = np.reshape(np_data[:,2], mesh_shape[::-1]).squeeze()
-#         # Each of these are indexed via [z,y,x], since x changes, then y, then z
-#         #   in the flattened array.
-#         return e_data_X, e_data_Y, e_data_Z, origin, spacing
-
